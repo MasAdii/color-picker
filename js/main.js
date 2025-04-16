750afdf1dda2a3384afade40153ed2373053cfae
masadii <githubmasadi@gmail.com>
2025-04-16 11:08:06 +0700

    Memberikan code pada file index.html, css dan js dan sudah responsif

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
- 
+document.getElementById('color').addEventListener('input', function(event){
+    let color = event.target.value
+
+    document.getElementById('codeWarna').textContent = color
+    document.getElementById('colorOutput').style.backgroundColor = color
+})
